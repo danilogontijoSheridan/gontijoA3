f9bc71fadb85693a6e3c7bd24266da9d45350623
danilogontijoSheridan <52791736+danilogontijoSheridan@users.noreply.github.com>
2019-07-25 13:33:21 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,10 @@
+import{
+    songInfo
+} from "../../app/songsInfo";
+
+export const MYSONGS : songInfo[] = [
+    { name : 'Wonderland', artist : 'Ananda Shake', genre : 'PsyTrance', year : 2008, picture : 'assets/images/ananda.jpeg'},
+    { name : 'Fade To Black', artist : 'Metallica', genre : 'Heavy Metal', year : 1989, picture : 'assets/images/metallica.png'},
+    { name : 'Riders On The Storm', artist : 'The Doors', genre : 'Rock', year : 1978, picture : 'assets/images/thedoors.jpg'},
+    { name : 'R U Still Down', artist : 'Tupac', genre : 'Hip-Hop', year : 1996, picture : 'assets/images/tupac.jpg'}
+]
